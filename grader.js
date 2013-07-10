2d991ceb994e04e78e4682248e870d5f54152a3c
Ubuntu <ubuntu@ip-172-31-26-101.us-west-2.compute.internal>
2013-07-10 23:04:32 +0000

    Added support for URL to grader.js

--- grader.js
+++ grader.js
@@ -3,29 +3,30 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(inFile) {
 	var inStr = inFile.toString();
 	if (!fs.existsSync(inStr)) {
-		console.log("%s does not exist. Exiting.", inStr);
+		console.error("%s does not exist. Exiting.", inStr);
 		process.exit(1);
 	}
 
 	return inStr;
 };
 
-var cheerioHtmlFile = function(htmlFile) {
-	return cheerio.load(fs.readFileSync(htmlFile));
+var cheerioHtml = function(html) {
+	return cheerio.load(html);
 };
 
 var loadChecks = function(checksFile) {
 	return JSON.parse(fs.readFileSync(checksFile));
 };
 
-var checkHtmlFile = function(htmlFile, checksFile) {
-	$ = cheerioHtmlFile(htmlFile);
+var checkHtml = function(html, checksFile) {
+	$ = cheerioHtml(html);
 	var checks = loadChecks(checksFile);
 	var out = {};
 	for (var ii in checks)
@@ -34,21 +35,70 @@ var checkHtmlFile = function(htmlFile, checksFile) {
 		out[checks[ii]] = present;
 	}
 
-	return out;
+	outputResults(out);
 };
 
 var clone = function(fn) {
 	return fn.bind({});
 };
 
+var buildWebCallbackFn = function(checksFile) {
+	var checkCallback = function(result, response) {
+		if (result instanceof Error) {
+			outputError(util.format(response.message));
+		} else {
+			checkHtml(result, checksFile);
+		}
+	};
+
+	return checkCallback;
+};
+
+var buildFsCallbackFn = function(checksFile) {
+	var checkCallback = function(err, data) {
+		if (err) {
+			outputError(err);
+		} else {
+			checkHtml(data, checksFile);
+		}
+	};
+
+	return checkCallback;
+};
+
+var outputError = function(err) {
+	console.error("Error occurred: %s", err);
+	program.exit(1);
+};
+
+var outputResults = function(resultJson) {
+	var outJson = JSON.stringify(resultJson, null, 4);
+	console.log(outJson);
+};
+
+var checkHtmlFile = function(htmlFile, checksFile) {
+	var checkCallback = buildFsCallbackFn(checksFile);
+	fs.readFile(htmlFile, checkCallback);
+};
+
+var checkUrl = function(url, checksFile) {
+	var checkCallback = buildWebCallbackFn(checksFile);
+	rest.get(url).on('complete', checkCallback);
+};
+
 if (require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-f, --file <html_file>', 'Path to html file', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <html_file_url>', 'URL of htm file')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+
+	if (program.url != null) {
+		checkUrl(program.url, program.checks);
+	} else {
+		checkHtmlFile(program.file, program.checks);
+	}
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
+	exports.checkUrl = checkUrl;
 }
